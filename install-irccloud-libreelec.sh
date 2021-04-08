e6ef6dd6fa1535c0fd499366ed360335a629b61b
Oscar Linderholm <osm@recv.se>
2021-04-08 18:50:09 +0200

    Add install irccloud script

---
+++ install-irccloud-libreelec.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo -n "irccloud email: "
+read email
+if [ -z "$email" ]; then
+	echo "invalid email"
+	exit 1
+fi
+
+echo -n "irccloud password: "
+read password
+if [ -z "$password" ]; then
+	echo "invalid password"
+	exit 1
+fi
+
+curl -s https://raw.githubusercontent.com/osm/scripts/master/irccloud.sh >/storage/irccloud.sh
+chmod 755 /storage/irccloud.sh
+
+crontab -l | grep -v irccloud.sh >/tmp/crontab
+echo "0 * * * * /storage/irccloud.sh $email $password" >>/tmp/crontab
+cat /tmp/crontab | crontab -
+rm /tmp/crontab
