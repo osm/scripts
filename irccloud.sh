57b1d43fb10b4812c254c7fa271adf1ac2570b14
Oscar Linderholm <osm@recv.se>
2021-05-02 17:31:51 +0200

    Bump user agent

--- irccloud.sh
+++ irccloud.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 	exit 1
 fi
 
-ua="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+ua="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
 
 form_token=$(curl \
 	-s \
